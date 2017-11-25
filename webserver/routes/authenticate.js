5a44b23c1a1b8443a99524a6e8a32490612d5301
Yiran Wang <boom.wang@mail.utoronto.ca>
2017-11-25 17:14:05 -0500

    added basic database and webserver pack TODO: more api

---
+++ webserver/routes/authenticate.js
@@ -0,0 +1,51 @@
+let express = require('express');
+let router = express.Router();
+let jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+let config = require('../config'); // get our config file
+let User = require('../models/user'); // get our mongoose model
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+
+    // find the user
+    User.findOne({
+        username: req.body.username
+    }, function(err, user) {
+
+        if (err) throw err;
+
+        if (!user) {
+            res.status(400);
+            res.json({ success: false, msg: 'Authentication failed. User not found.' });
+        } else if (user) {
+
+            // check if password matches
+            if (user.password !== req.body.password) {
+                res.status(400);
+                res.json({ success: false, msg: 'Authentication failed. Wrong password.' });
+            } else {
+
+                // if user is found and password is right
+                // create a token
+                let payload = {
+                    admin: user.admin,
+                    username: user.username
+                };
+                let token = jwt.sign(payload, config.superSecret, {
+                    expiresIn: 8640000 // expires in 2400 hours
+                });
+
+                res.json({
+                    success: true,
+                    msg: 'Token recieved',
+                    token: token
+                });
+            }
+
+        }
+
+    });
+
+});
+
+module.exports = router;
