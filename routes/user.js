4b83f02c90386bfa1919b030ad7a7f24ef341dbe
luchenya <chenyang.lu@mail.utoronto.ca>
2017-11-30 21:10:05 -0500

    finish remove current user

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
+let User = require('../models/user');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-
-    res.send('Hello ' + req.decoded.username + "!");
-});
-
-router.get('/msg', function(req, res, next) {
-
-    res.send("!!");
+router.delete('/', function (req, res, next) {
+    let username = req.decoded.username;
+    console.log(username);
+    User.remove({username: username}, function (err) {
+        if (err){
+            res.json({success: false})
+        }
+        res.clearCookie("token", { path: '/' });
+        res.json({success: true});
+    });
 });
 
 module.exports = router;
