add2fd90f9f61315f2881788179e2ba29defd6f7
LesterLyu <lvds2000@gmail.com>
2018-03-12 00:57:35 -0400

    Deploy to Godaddy shared host.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    database:"mongodb://99.229.227.21/csc309",
+    database:"mongodb://127.0.0.1/whatToEat",
     superSecret:"fdg340954togmnr93wsdmcv-3wr-2- ds f:<>/ew",
     googleWebServiceKey: 'AIzaSyCka6hdwwJXh_y_zDAorNNJ36YL6UoBR3A',
     yelpAccessToken: "bI2YLZdX17shl9wjvoJf3mG0kWSWD0KN7TXeD4JRj6bH8PPselrY0tbFM6H-l7eAM4-TZqk0Mo3NxTevH0BiSub_-Q47v_ZN5qCTAGupUDYwqY2M8T2lNeu4bsX0WXYx",
